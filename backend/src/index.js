4e1dc4cbc4612a7eb6c7ae718dd780ab32c47e51
Naiara <naiarazk@gmail.com>
2020-03-27 21:57:00 -0300

    Backend e Frontend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
